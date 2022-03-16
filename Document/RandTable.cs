8a80499efc3c10bb3ec3fabf214586c4da9845b7
svetaallo <hellosedaya@gmail.com>
2022-03-16 14:59:58 +0500

    Initial commit

---
+++ Document/RandTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace DocRandomizer
+{
+    class RandTable
+    {
+        private static Random random = new Random();
+        private const string RandomizerInputDirectory = @"C:\DocRandomizer\частицы2022.docx";
+        private const string RandomizerOutputDirectory = @"C:\DocRandomizer\";
+        static RandTable()
+        {
+            if (!Directory.Exists(RandTable.RandomizerOutputDirectory))
+            {
+                Directory.CreateDirectory(RandTable.RandomizerOutputDirectory);
+                var doc = DocX.Load(Program.DocumentDirectory + @"частицы2022.docx");
+                doc.SaveAs(RandTable.RandomizerOutputDirectory + @"частицы2022.docx");
+            }
+        }
+        public static void Do()
+        {
+            var document = DocX.Load(RandTable.RandomizerInputDirectory);
+            var table = document.Tables[1];
+
+            for (int i = 1; i < 6; i++)
+            {
+                table.Rows[i].Cells[3].Paragraphs[0].RemoveText(0);
+                table.Rows[i].Cells[3].Paragraphs[0].Append(random.Next(90000, 210000).ToString());
+                table.Rows[i].Cells[4].Paragraphs[0].RemoveText(0);
+                table.Rows[i].Cells[4].Paragraphs[0].Append(random.Next(500, 1700).ToString());
+            }
+            document.SaveAs(RandTable.RandomizerOutputDirectory + @"частицы2022randomized.docx");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace DocRandomizer
+{
+    class Program
+    {
+#if NETCORE || NET5
+    internal const string DocumentDirectory = @"..\..\..\..\Document\";
+#else
+        internal const string DocumentDirectory = @"..\..\..\Document\";
+#endif
+
+
+        static void Main(string[] args)
+        {
+            RandTable.Do();
+            Console.WriteLine("Готово!");
+        }
+
+
+    }
+}
